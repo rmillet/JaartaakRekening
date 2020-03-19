6cfbe3b97c52801d2e53622a7b38216ece9d6947
Ronny Millet <rmillet@imb-brugge.net>
2020-03-19 11:01:27 +0100

    Kadootje om te starten

---
+++ JaartaakRekening-Business/Verrichting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaartaakRekening_Business
+{
+    public class Verrichting
+    {
+        //de private fields
+
+        private double _bedrag;
+        private DateTime _datum;
+        private string _omschrijving;
+
+
+
+        //methode (override)
+
+        public override string ToString()
+        {
+            return _omschrijving + " (" + _bedrag.ToString("C") + " )  op " + _datum.ToString("d");
+        }
+    }
+}
